8c29cc16c6e7e71b5bc2f545c557e9134a9e5403
Max Ross <maxymus.ross@gmail.com>
2020-04-29 13:58:10 -0600

    set up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['react', 'es2015']
+            presets: ['@babel/react', '@babel/env']
           }
         }
       }
