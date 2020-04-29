8bfae8132566afd4e09c3fa010488ca96ce5435c
Max Ross <maxymus.ross@gmail.com>
2020-04-29 13:53:08 -0600

    set up directories

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  output: {
+    path: __dirname + '/client/dist'
+    },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      }
+    ]
+  }
+};
